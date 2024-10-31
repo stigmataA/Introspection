173fb9f13d11c78d4cb55951566b9629cb8a082e
stigmataA <r.muhutdinov@gmail.com>
2024-10-31 15:12:00 +0300

    Add files via upload

---
+++ Introspection.py
@@ -0,0 +1,16 @@
+def introspection_info(obj):
+    obj_type = type(obj).__name__ # определение типа объекта
+    attributes = dir(obj) # получение списка атрибутов объекта
+    methods = [method for method in attributes if callable(getattr(obj, method))] # получение методов объекта
+    module = obj.__class__.__module__ # получение имени модуля объекта
+    info = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module} # создание словаря с информацией о объекте
+
+    return info
+
+
+number_info = introspection_info(42) # получение информации о числе
+print(number_info)
+string_info = introspection_info('Hello World') # получение информации о строке
+print(string_info)
+list_info = introspection_info([1, 20, 4.0, 'hello']) # получение информации о списке
+print(list_info)
